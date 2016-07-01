da906fbf451e82cee2d3059a1e82e6376f92882f
Fabien CÔTÉ <fabien.cote@me.com>
2016-07-01 10:40:32 +0200

    update settings

--- server/settings/index.js
+++ server/settings/index.js
@@ -2,10 +2,7 @@ var yargs = require('yargs');
 
 module.exports = function (app) {
 	var DEFAULT_ENV = 'development';
-	var env = yargs
-				  .alias('e', 'env')
-				  .argv
-				  .env || DEFAULT_ENV;
+	var env = yargs.alias('e', 'env').argv.env || process.env.NODE_ENV || DEFAULT_ENV;
 
 	app.settings = require('./' + env);
 };
