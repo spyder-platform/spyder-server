4a72b9146675f2bd62063c7323e0271315d08acc
Fabien CÔTÉ <fabien.cote@me.com>
2016-07-16 20:32:07 +0200

    add (Movement) : not found error on show action

--- server/actions/movement/show.js
+++ server/actions/movement/show.js
@@ -5,6 +5,14 @@ module.exports = function (app) {
 		app.models.Movement.findOne({
 			_id: req.params.id
 		}).then(function(movement) {
+			if(!movement) {
+				return res.status(404).send({
+					success: false,
+					data: null,
+					message: 'Cette photo est introuvable.'
+				});
+			}
+
 			res.sendFile(path.resolve('storage') + '/' + movement.path);
 		})
 	}
